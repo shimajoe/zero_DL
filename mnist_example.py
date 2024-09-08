dcb022c9fa5342801ee940d1c8b63715c7ead8bd
Joichiro Shimada <shima.joe0427@gmail.com>
2024-09-08 18:07:45 +0900

    MNIST画像出力

--- mnist_example.py
+++ mnist_example.py
@@ -1,12 +1,29 @@
 import sys, os
 sys.path.append(os.pardir) # 親ディレクトリのファイルをインポートするための設定
+import numpy as np
 from dataset.mnist import load_mnist
+from PIL import Image # 画像出力のライブラリ
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
 
 # 最初の呼び出しは数分待つ
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
-# それぞれのデータの形状を出力
-print(x_train.shape)
-print(t_train.shape)
-print(x_test.shape)
-print(t_test.shape)
+# # それぞれのデータの形状を出力
+# print(x_train.shape) # (60000, 784)
+# print(t_train.shape) # (60000,)
+# print(x_test.shape) # (10000, 784)
+# print(t_test.shape) # (10000,)
+
+# MNIST画像出力
+img = x_train[0]
+label = t_train[0]
+print(label)
+
+print(img.shape) # (784,)
+img = img.reshape(28,28) # 形状を元の画像サイズに変形
+print(img.shape) # (28,28)
+
+img_show(img)
